c5c023cc176f9e55a5cb1bcdde86e59a1c715acd
bichiko <b_kodua@cu.edu.ge>
2019-03-08 16:07:21 +0200

    text updated

--- webserver/questions.php
+++ webserver/questions.php
@@ -6,7 +6,7 @@ if(file_exists('../../flag.txt')){
 }
 // echo ROOT_URL.'home/ajax';
 return array(
-	'description' => 'In this challenge lab you need to answer all the question which can be found in Flag submission tab.',
+	'description' => 'In this challenge lab you need to answer all the question which can be found in Flag submission tab. <br>If you face any technical problem here, you can always try to open lab (<b>192.168.8.254</b>) in <b>fireforx</b>.',
 	'questions' => [
 		'What is the HTML field name which is vulnerable for the SQL injection?',
 		'What is the full URL of ajax request while searching the movies?',
