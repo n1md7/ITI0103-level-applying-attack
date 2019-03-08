65c230adfb626528d11d03e7c051b3a4ec6d9d9f
bichiko <b_kodua@cu.edu.ge>
2019-03-08 14:53:53 +0200

    fix

---
+++ webserver/questions.php
@@ -0,0 +1,29 @@
+<?php
+if(file_exists('../../flag.txt')){
+    $flag = trim(file_get_contents('../../flag.txt'));
+}else if(file_exists('/var/www/flag.txt')){
+    $flag = trim(file_get_contents('/var/www/flag.txt'));
+}
+// echo ROOT_URL.'home/ajax';
+return array(
+	'description' => 'In this challenge lab you need to answer all the question which can be found in Flag submission tab.',
+	'questions' => [
+		'What is the HTML field name which is vulnerable for the SQL injection?',
+		'What is the full URL of ajax request while searching the movies?',
+		'How many movie records are in database?',
+		'Find a secret movie names and submit the flag'
+	],
+	'answers' => [
+		'search',
+		ROOT_URL.'home/ajax',
+		"7",
+		$flag
+	],
+	'hints' => [
+		'Flag. Format is HTB{...}',
+		'Try 2 Google 😁'
+	]
+);
+
+
+
--- webserver/views/_templates/main.php
+++ webserver/views/_templates/main.php
@@ -11,10 +11,14 @@
     <script src="<?php echo ROOT_PATH; ?>assets/md/js/jquery-3.2.1.min.js"></script>
     <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
     <script src="<?php echo ROOT_PATH; ?>assets/md/js/popper.min.js"></script>
+    <!-- Optional JavaScript -->
     <meta content="<?php echo $csrf ?>" name="csrf">
     <title><?php echo TITLE; ?></title>
   </head>
   <body>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?php echo ROOT_PATH; ?>assets/md/js/bootstrap.min.js"></script>
+    <script src="<?php echo ROOT_PATH; ?>assets/md/js/mdb.min.js"></script>
   <!-- nav start -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#">SQLi</a>
@@ -63,12 +67,5 @@
       <?php require('./views/footer.html'); ?>
     </div>
     
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="<?php echo ROOT_PATH; ?>assets/md/js/bootstrap.min.js"></script>
-    <script src="<?php echo ROOT_PATH; ?>assets/md/js/mdb.min.js"></script>
-    <script type="text/javascript">
-  // $('#labInfoBtn').modal('show');
-</script>
   </body>
 </html>
